0d8427f07bea97449c07e425a079151b5e00f9a6
AttaMuhammad16 <attagit1639@gmail.com>
2024-08-21 21:47:28 +0500

    calculator app

---
+++ app/src/main/java/com/mathcal/calculatorapp/SplashScreen.kt
@@ -0,0 +1,26 @@
+package com.mathcal.calculatorapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+        lifecycleScope.launch {
+            delay(4000)
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+        }
+
+    }
+}
